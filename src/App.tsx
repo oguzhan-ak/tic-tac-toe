7704a2801ac66473f74daa206d6bbe792d5c40c7
oguzhan-ak <oguzhan_ak@outlook.com.tr>
2022-08-16 23:21:57 +0300

    gereksiz yerler islindi

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import "./App.css";
 import BigBox from "./components/bigBox/bigBox";
 
 function App() {
-  return <BigBox></BigBox>;
+  return (
+    <>
+      <BigBox></BigBox>
+    </>
+  );
 }
 
 export default App;
--- src/components/bigBox/bigBox.tsx
+++ src/components/bigBox/bigBox.tsx
@@ -17,7 +17,6 @@ const BigBox = (props: any) => {
           );
         })}
       </div>
-      ;
     </>
   );
 };
